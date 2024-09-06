7b78c75307614ac302983f4dbef6798b38ead3e3
Parul Agarwal <polk54@gmail.com>
2024-09-05 22:43:30 -0400

    Update jacket.js

--- jacket.js
+++ jacket.js
@@ -5,6 +5,6 @@ function getJacket() {
 }
 
 var randomJacket = new Array();
-randomJacket[0] = "https://photos.google.com/share/AF1QipP20ERqpL6Ky4qIAAYsfm0qMF6qca39zLoFjRXn7e8aBLAGYNxzskEWRwYikbZl6g/photo/AF1QipMB4e-KcuuSvh1QC2A0u6gAVelukPAhgelWYjKV?key=OC1aRUEyLV9uVXA2LTZCbGxsRzdPZGx0YWx3cTJ3";
-randomJacket[1] = "https://photos.google.com/share/AF1QipP20ERqpL6Ky4qIAAYsfm0qMF6qca39zLoFjRXn7e8aBLAGYNxzskEWRwYikbZl6g/photo/AF1QipMB4e-KcuuSvh1QC2A0u6gAVelukPAhgelWYjKV?key=OC1aRUEyLV9uVXA2LTZCbGxsRzdPZGx0YWx3cTJ3";
+randomJacket[0] = "https://s7d5.scene7.com/is/image/Anthropologie/4133380630020_090_b";
+randomJacket[1] = "https://s7d5.scene7.com/is/image/Anthropologie/4133380630020_090_b";
 randomJacket[2] = "https://s7d5.scene7.com/is/image/Anthropologie/4133380630020_090_b";
